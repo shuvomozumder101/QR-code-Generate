55c36f62618327b35326704de825da17d90be95a
shuvo <shuvomozumder101@gmail.com>
2021-09-07 17:50:23 +0600

    QR code

---
+++ QR_Cord/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QR_Cord.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QR_Cord.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateQRCode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateQRCode(QRCodeModel qRCode)
+        {
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(qRCode.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(10);
+            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
+            ViewBag.QrCodeUri = QrUri;
+            return View();
+        }
+    }
+
+    public static class BitmapExtension
+    {
+        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+    }
+}
---
+++ QR_Cord/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QR_Cord.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
